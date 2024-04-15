2a193232cc65ab3e581a5d9a685d5a77d2b41929
wahab omodolapo <dolwithneybright1@gmail.com>
2024-04-15 20:13:30 +0100

    ,,

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const mongodb_url =  process.env.MONGODB_URI
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://oreoluwa212:MONadeJames1234@cluster0.l73rzz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 ")
+    const conn = await mongoose.connect(mongodb_url)
     console.log(`Database Connected: ${conn.connection.host}`)
   } catch (err) {
     console.error(`Error: ${err.message}`)
